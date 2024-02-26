9d8bc340edb2f0d83ba0b07d8232bf131853234f
Lydia <guipopo5@gmail.com>
2024-02-26 20:41:46 +0800

    OK

---
+++ Project8-7 Swifty Words/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Project8-7 Swifty Words
+//
+//  Created by Lydia Lu on 2024/2/26.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+     
+        
+        }
+    
+  
+    
+    }
+    
+ 
